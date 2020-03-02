5925a6b5bead76bce68a2778a7ab3e27dbe9bcde
clicktronix <s28.morgan@gmail.com>
2020-03-03 00:05:10 +0600

    Base project implementation

---
+++ .eslintrc.js
@@ -0,0 +1,78 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['airbnb-typescript'],
+  parserOptions: {
+    project: `./tsconfig.json`
+  },
+  rules: {
+    'react/jsx-props-no-spreading': 'off',
+    'react/state-in-constructor': ['error', 'never'],
+    'react/no-array-index-key': 'off',
+    'react/prop-types': 'off',
+    'react/no-danger': 'error',
+    'react/static-property-placement': 'off',
+    'react/destructuring-assignment': ['error', 'always', { ignoreClassFields: true }],
+    'react/button-has-type': 'off',
+    'react/sort-comp': [
+      'error',
+      {
+        order: [
+          'static-variables',
+          'static-methods',
+          'instance-variables',
+          'getters',
+          'setters',
+          'lifecycle',
+          'render',
+          'instance-methods',
+          'everything-else',
+        ],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': 'error',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/type-annotation-spacing': [
+      'error',
+      {
+        before: false,
+        after: true,
+        overrides: { arrow: { before: true, after: true } },
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        'groups': [['builtin', 'external'], 'internal', ['parent', 'sibling'], 'index'],
+        'newlines-between': 'always',
+      },
+    ],
+    '@typescript-eslint/no-unused-expressions': ['error', { allowShortCircuit: true, allowTernary: true }],
+    'no-console': ['error', { allow: ['info', 'warn', 'error'] }],
+    'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
+    'max-len': ['error', { code: 120, ignoreComments: true }],
+    'indent': ['error', 2, { ObjectExpression: 1 }],
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: { minProperties: 2 },
+      },
+    ],
+  },
+  env: {
+    jest: true,
+    browser: true,
+    node: true,
+  },
+  globals: {
+    window: 'readonly',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['./src'],
+      },
+    },
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+};
